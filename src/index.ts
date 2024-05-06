257fdd57f56c8aa44959cf1db7a96352c70a6f4c
gaurav-9896 <panchalgaurav487@gmail.com>
2024-05-06 14:22:10 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,13 @@ const CSS_URL =
   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 app.use(
   "/api-docs",
   swaggerUi.serve,
   swaggerUi.setup(SwaggerDocument, { customCssUrl: CSS_URL })
 );
+app.use(cors());
+app.use(express.json());
 
 connectDB();
 
